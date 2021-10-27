73c7866cc3b7240d3c61a4b9605758b2c5b36334
VasilevVV <81243152+VasilevVV@users.noreply.github.com>
2021-10-27 17:40:12 +0700

    Начало работы над ЛБ2

---
+++ LB1/TPU.LB1.PersonLibrary/Adult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPU.LB1.PersonLibrary
+{
+    
+    public class Adult : Person
+    {
+        public int PassportNumber { get; set; }
+
+        public Adult Partner { get; set; }
+
+        public bool IsMarried
+        {
+            get
+            {
+                return (Partner != null);
+            }
+        }
+
+        public string Job { get; set; }
+
+
+
+        public Adult(string firstName, string lastName, int age,
+            Gender gender, int passportNumber) : base(firstName, lastName, age, gender)
+        {
+            PassportNumber = passportNumber;
+        }
+
+
+    }
+}
