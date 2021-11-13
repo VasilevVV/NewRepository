0c245cbdf5625279eb3ce1ecaa8653bb849120a6
VasilevVV <81243152+VasilevVV@users.noreply.github.com>
2021-11-13 14:12:39 +0700

    ЛБ3 готова (есть лишний класс ThingToBay)

--- ConsoleLoader/Program.cs
+++ ConsoleLoader/Program.cs
@@ -7,8 +7,16 @@ using Model;
 
 namespace ConsoleLoader
 {
-    class Program
+    /// <summary>
+    /// Тестирование библиотеки классов Model
+    /// </summary>
+    public class Program
     {
+        /// <summary>
+        /// Действия для формирования скидки
+        /// </summary>
+        /// <param name="readDiscount">скидка для формирования</param>
+        /// <returns>Сформированная скидка</returns>
         public static IDiscount ReadDiscount(IDiscount readDiscount)
         {
             Action[] actionsArray = new Action[]
@@ -16,37 +24,28 @@ namespace ConsoleLoader
                 () =>
                 {
                     Console.WriteLine("Введите величину скидки");
-                    readDiscount.DiscountValue = float.Parse(Console.ReadLine());
+                    readDiscount.DiscountValue = 
+                                        float.Parse(Console.ReadLine());
                 },
                 () =>
                 {
-                    Console.WriteLine("Введите магазин/предприятие/организацию/");
+                    Console.WriteLine("Введите магазин/предприятие/" +
+                                "организацию/компанию выдавшего скидку");
                     readDiscount.Company = Console.ReadLine();
                 },
-                () =>
-                {
-                    Console.WriteLine("Введите возраст");
-                    personRead.Аge = Convert.ToInt32(Console.ReadLine());
-                },
-                () =>
-                {
-                    Console.WriteLine("Введите пол м/ж");
-                    string gender = Console.ReadLine();
-                    switch (gender)
-                    {
-                        case "м":
-                            personRead.Gender = Gender.Male;
-                            break;
-                        case "ж":
-                            personRead.Gender = Gender.Female;
-                            break;
-                        default:
-                            throw new ArgumentException("Некорректно" +
-                                " введён пол.");
-                    }
-                }
             };
-                        
+
+            MakeActions(actionsArray);
+
+            return readDiscount;
+        }
+
+        /// <summary>
+        /// Реализовать действия
+        /// </summary>
+        /// <param name="actionsArray">Массив реализуемых действий</param>
+        public static void MakeActions(Action[] actionsArray)
+        {
             foreach (var action in actionsArray)
             {
                 while (true)
@@ -58,11 +57,58 @@ namespace ConsoleLoader
                     }
                     catch (Exception ex)
                     {
-                        Console.WriteLine($"{ex.Message}\nПопробуйте снова.");
+                        Console.WriteLine($"{ex.Message}\n" +
+                                          $"Попробуйте снова.");
                     }
                 }
             }
-            return readDiscount;
+        }
+
+        /// <summary>
+        /// Действия для формирования срока действия скидок
+        /// </summary>
+        /// <param name="discountPeriod">период для формирования</param>
+        /// <returns>Сформированный период</returns>
+        public static DiscountPeriod GetPeriodForDiscount
+                                     (DiscountPeriod discountPeriod)
+        {
+            Action[] actionsArray = new Action[]
+            {
+                () =>
+                {
+                    Console.WriteLine("Введите дату начала действия " +
+                                      "скидки в форматах:\n" +
+                                      "--ДД.ММ.ГГГГ ЧЧ:ММ:СС--\n" +
+                                      "--ДД.ММ.ГГГГ ЧЧ:ММ--\n" +
+                                      "--ДД.ММ--\n" +
+                                      "Или введите: now, сейчас -" +
+                                      " чтобы получить текущее время");
+                    string readString = Console.ReadLine();
+                    if (readString == "now" || readString == "сейчас")
+                    {
+                        discountPeriod.DateTimeDiscountStart = DateTime.Now;
+                    }
+                    else
+                    {
+                        discountPeriod.DateTimeDiscountStart =
+                                        DateTime.Parse(Console.ReadLine());
+                    }
+                },
+                () =>
+                {
+                    Console.WriteLine("Введите дату окончания действия " +
+                                      "скидки в форматах:\n" +
+                                      "--ДД.ММ.ГГГГ ЧЧ:ММ:СС--\n" +
+                                      "--ДД.ММ.ГГГГ ЧЧ:ММ--\n" +
+                                      "--ДД.ММ--");
+                    discountPeriod.DateTimeDiscountEnd =
+                                        DateTime.Parse(Console.ReadLine());
+                },
+            };
+
+            MakeActions(actionsArray);
+            
+            return discountPeriod;
         }
 
         static void Main(string[] args)
@@ -71,7 +117,9 @@ namespace ConsoleLoader
                 "СКИДКИ, СКИДКИ, СКИДКИ!!! ВСЕМ СКИДКИ!!! " +
                 "И НА ТУ, И НА ЭТУ, И НА ВОН ТУ!!!\n" +
                 "Нажмите любую кнопку, чтобы начать...");
-            Console.ReadLine();
+            Console.ReadKey();
+
+            List<IDiscount> discounts = new List<IDiscount>();
 
             while (true)
             {
@@ -85,43 +133,108 @@ namespace ConsoleLoader
                                   "!!!на ЛЮБУЮ сумму!!!");
                 Console.WriteLine("4 - Временную скидку по сертификату " +
                                   "!!!на ЛЮБУЮ сумму!!!");
-                Console.WriteLine("5 - Вам не нужны скидки? " +
-                                  "Вы теряете свой уникальный шанс разбогатеть");
+                Console.WriteLine("5 - Заканчиваем со скидками " +
+                                  "и переходим к покупке!");
+                Console.WriteLine("6 - Вам не нужны скидки? " +
+                                  "Вы теряете свой уникальный шанс " +
+                                  "разбогатеть");
                 Console.WriteLine("");
                 var consoleKey = Console.ReadLine();
-
-
 
                 switch (consoleKey)
                 {
                     case "1":
                         {
-                            ReadDiscount();
+                            Console.WriteLine("Отличный выбор: " +
+                                "бессрочная процентная скидка");
+                            ProcentDiscountForever discount = 
+                                                new ProcentDiscountForever();
+                            discount = 
+                              (ProcentDiscountForever)ReadDiscount(discount);
+                            discounts.Add(discount);
                             break;
                         }
                     case "2":
                         {
-                            Console.WriteLine("Пирожочек");
+                            Console.WriteLine("Отличный выбор: " +
+                                "временная процентная скидка");
+                            ProcentDiscountForPeriod discount = 
+                                              new ProcentDiscountForPeriod();
+                            discount = (ProcentDiscountForPeriod)
+                                        ReadDiscount(discount);
+                            Console.WriteLine("");
+                            discount.Period = 
+                                       GetPeriodForDiscount(discount.Period);
+                            discounts.Add(discount);
                             break;
                         }
                     case "3":
                         {
-                            Console.WriteLine("Вареник");
+                            Console.WriteLine("Отличный выбор: " +
+                                "бессрочная скидка по сертификату");
+                            CertificateDiscountForever discount = 
+                                            new CertificateDiscountForever();
+                            discount = (CertificateDiscountForever)
+                                        ReadDiscount(discount);
+                            discounts.Add(discount);
                             break;
                         }
                     case "4":
                         {
-                            Console.WriteLine("Катлетка");
+                            Console.WriteLine("Отличный выбор: " +
+                                "временная скидка по сертификату");
+                            CertificateDiscountForPeriod discount = 
+                                          new CertificateDiscountForPeriod();
+                            discount = (CertificateDiscountForPeriod)
+                                        ReadDiscount(discount);
+                            Console.WriteLine("");
+                            discount.Period = 
+                                       GetPeriodForDiscount(discount.Period);
+                            discounts.Add(discount);
                             break;
                         }
                     case "5":
                         {
-                            Environment.Exit(0);
+                            Console.WriteLine("Применим скидки\n" +
+                                              "Введите цену товара");
+                            float originalPrice = 0.0f;
+                            bool flag = true;
+                            while (originalPrice == 0.0f)
+                            {
+                                try
+                                {
+                                    originalPrice = 
+                                             float.Parse(Console.ReadLine());
+                                    flag = false;
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"{ex.Message}\n" +
+                                                      $"Попробуйте снова.");
+                                }
+                            }
+                            float priceAllDiscounts = originalPrice;
+                            var ie = discounts.GetEnumerator();
+                            while (ie.MoveNext())
+                            {
+                                priceAllDiscounts = 
+                                      ie.Current.GetPrice(priceAllDiscounts);
+                            }
+                            ie.Dispose();
+                            Console.WriteLine($"Отлично!!!\n" +
+                                $"Теперь цена {priceAllDiscounts}\n" +
+                                $"вместо {originalPrice}");
+                            discounts.Clear();
                             break;
+                        }
+                    case "6":
+                        {
+                            return;
                         }
                     default:
                         {
-                            Console.WriteLine("Мой дорогой, попробуйте что-нибудь другое");
+                            Console.WriteLine("Мой дорогой, " +
+                                "попробуйте что-нибудь другое");
                             Console.WriteLine("");
                             break;
                         }
